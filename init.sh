a39263598fe51ad58d3f0aa03684eb1960001456
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2021-12-08 21:05:34 +0100

    improve init script

--- init.sh
+++ init.sh
@@ -1,8 +1,17 @@
 source .env
 
+clean()
+{
+  docker exec "${API_CONTAINER_NAME}" rm -r /usr/local/var/www/api
+}
+
+init_api() {
+  docker exec -w /usr/local/var/www/ "${API_CONTAINER_NAME}" /usr/local/etc/php/composer create-project symfony/skeleton api
+}
+
 docker-compose up -d --build
 
-docker exec "${API_CONTAINER_NAME}" rm -r /usr/local/var/www/api
+clean
 
-docker exec -w /usr/local/var/www/ "${API_CONTAINER_NAME}" /usr/local/etc/php/composer create-project symfony/skeleton api
+init_api
 
