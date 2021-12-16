704884d438da8402d390fc5d5195ac6c50408e68
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2021-12-16 20:11:57 +0100

    improve nginx docker config

--- init.sh
+++ init.sh
@@ -1,17 +1,6 @@
 source .env
 
-clean()
-{
-  docker exec "${API_CONTAINER_NAME}" rm -r /usr/local/var/www/api
-}
-
-init_api() {
-  docker exec -w /usr/local/var/www/ "${API_CONTAINER_NAME}" /usr/local/etc/php/composer create-project symfony/skeleton api
-}
-
 docker-compose up -d --build
-
-clean
 
 init_api
 
